006ba10b928672f623ae5d1fb7aaf2ecb4c8a881
Muka <dima040805@yandex.ru>
2025-09-15 02:34:03 +0300

    try

--- app/store/admin/accessor.py
+++ app/store/admin/accessor.py
@@ -1,7 +1,10 @@
 from typing import TYPE_CHECKING
 
+from sqlalchemy import select, insert
+
 from app.admin.models import AdminModel
 from app.base.base_accessor import BaseAccessor
+from app.web.utils import hash_password
 
 if TYPE_CHECKING:
     from app.web.app import Application
@@ -9,10 +12,27 @@ if TYPE_CHECKING:
 
 class AdminAccessor(BaseAccessor):
     async def connect(self, app: "Application") -> None:
-        raise NotImplementedError
+        self.app = app
 
     async def get_by_email(self, email: str) -> AdminModel | None:
-        raise NotImplementedError
+        request = select(AdminModel)
+        async with self.app.database.session() as session:
+            res = await session.execute(request)
+
+            for admin in res:
+                if admin.email == email:
+                    return AdminModel(id=1, email=admin.email, password=hash_password(admin.password))
+
+        if self.app.config.admin.email == email:
+            return AdminModel(id=1, email=self.app.config.admin.email,
+                              password=hash_password(self.app.config.admin.password))
+
+        return None
 
     async def create_admin(self, email: str, password: str) -> AdminModel:
-        raise NotImplementedError
+        request = insert(AdminModel).values(email=email, password=hash_password(password))
+        async with self.app.database.session as session:
+            res = await session.execute(request)
+            res.all()
+
+        return AdminModel(email=email, password=password)
--- app/store/database/database.py
+++ app/store/database/database.py
@@ -1,9 +1,10 @@
 from typing import TYPE_CHECKING, Any
 
+from sqlalchemy import URL
 from sqlalchemy.ext.asyncio import (
     AsyncEngine,
     AsyncSession,
-    async_sessionmaker,
+    async_sessionmaker, create_async_engine,
 )
 from sqlalchemy.orm import DeclarativeBase
 
@@ -22,14 +23,18 @@ class Database:
         self.session: async_sessionmaker[AsyncSession] | None = None
 
     async def connect(self, *args: Any, **kwargs: Any) -> None:
-        raise NotImplementedError
-        # self.engine = create_async_engine(
-        #     URL.create(
-        #     ),
-        # )
-        # self.session = async_sessionmaker(
-        #
-        # )
+        self.engine = create_async_engine(
+            URL.create(
+                drivername="postgresql+asyncpg",
+                username="kts_user",
+                password="kts_pass",
+                host="localhost",
+                database="kts"
+            ),
+        )
+        self.session = async_sessionmaker(
+            self.engine, expire_on_commit=False
+        )
 
     async def disconnect(self, *args: Any, **kwargs: Any) -> None:
-        raise NotImplementedError
+        await self.engine.dispose()
